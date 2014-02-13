3c28e45a6c75a93b47c12d1a09c419ff5796157f
Oliver Switzer <oliverswitzer@gmail.com>
2014-02-13 18:54:33 -0500

    add custom parallax to pickup page and section coloring

--- public/app/js/pickups.js
+++ public/app/js/pickups.js
@@ -1,11 +1,24 @@
 // custom_parallax.js
 
+$(function() {
+  var $sections = $('.section'),
+      $bgimg = $('#angular-background'),
+      $sectionContainer = $('#section_container'),
+      windowHeight = $(window).height();
 
-console.log("WHAT")
-$(window).scroll(function() {   //for parallax effect on hero
-  var $bgimg = $('#angular-background');
-  var yPos = -($(this).scrollTop() / 5);
-  var coords = '50% '+ yPos + 'px';
-  $bgimg.css("background-position", coords);
-  console.log($bgimg.css("background-position"))
-});
+  console.log(windowHeight);
+  $sectionContainer.css("margin-top", windowHeight)
+
+  $.each([$sections, $bgimg], function() {
+    $(this).css("height", windowHeight);
+  })
+
+
+  //custom parallax
+  $(window).scroll(function() {   //for parallax effect on hero
+    var $bgimg = $('#angular-background');
+    var yPos = -($(this).scrollTop() / 5);
+    var coords = '50% '+ yPos + 'px';
+    $bgimg.css("background-position", coords);
+  }); 
+})

6e321b88b60495ae1d4e6dd846192bbf25e9ce55
Oliver Switzer <oliverswitzer@gmail.com>
2014-02-25 21:14:48 -0500

    move environments.rb content into app.rb

--- app.rb
+++ app.rb
@@ -1,6 +1,5 @@
 require 'bundler'
 Bundler.require
-require './environments'
 require 'sinatra/activerecord'
 
 Dir["./lib/*.rb"].each {|file| require file }
@@ -14,6 +13,25 @@ set :sockets, []
 module Name
   class App < Sinatra::Application
 
+
+    configure :development do 
+      set :database, "sqlite3:///database.db"
+    end
+
+
+    configure :production do
+      db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+       ActiveRecord::Base.establish_connection(
+         :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+         :host     => db.host,
+         :username => db.user,
+         :password => db.password,
+         :database => db.path[1..-1],
+         :encoding => 'utf8'
+       )
+    end
+    
     configure do
       set :root, File.dirname(__FILE__)
       set :public_folder, 'public/app'

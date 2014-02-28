40bd8f65b723f73a3982042f51737c5612190135
Kphillycat <keithwilliamsjr@gmail.com>
2014-02-28 11:27:40 -0500

    Add mailcatcher and mail to send test mails

--- lib/mail.rb
+++ lib/mail.rb
@@ -8,12 +8,18 @@ class Confirmation
 
     SidekiqWorker.perform_async("bob", 5)
 
-    # mail = Mail.new do
-    #   from    'mikel@test.lindsaar.net'
-    #   to      'you@test.lindsaar.net'
-    #   subject 'This is a test email'
-    #   body    File.read('body.txt')
-    # end
+    mail = Mail.new do
+      from    'keithwilliamsjr@gmail.com'
+      to      'oliverswitzer@gmail.com'
+      subject 'Worky workie'
+      body    "IT WORKS!"
+    end
+
+    Mail.defaults do
+      delivery_method :smtp, address: "localhost", port: 1025
+    end
+
+    mail.deliver
   end
 
 end
